11078b065f4c9fb8a60b00d32db37b5259519cec
Praveen Mishra <helloworld@gmail.com>
2018-07-10 10:16:11 +0530

    swagger issue resolved by disabling DefaultController

--- open-commons-rest/src/main/java/org/openframework/common/rest/DefaultController.java
+++ open-commons-rest/src/main/java/org/openframework/common/rest/DefaultController.java
@@ -3,14 +3,19 @@ package org.openframework.common.rest;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by lcsontos on 5/10/17.
+ * Created by lcsontos on 5/10/17. This DefaultController is not OK if any non
+ * spring controller url is requested, because this DefaultController blocks the
+ * non spring controller url and creates an error response. For example, swagger
+ * integration. Swagger has the URL:
+ * http://localhost:8080/<contextPath>/api/swagger-ui.html. This is blocked by
+ * this DefaultController, so removing it.
  */
-@Controller
+//@Controller
 public class DefaultController {
 
 	@RequestMapping
